fafbaa5f921ccf8254661573d75ee53bd45da743
Nastenko Peter <source-2306@mail.ru>
2025-02-13 12:10:09 +0300

    Добавлены данные для входа в бд

--- config.py
+++ config.py
@@ -9,5 +9,12 @@ DB_PARAMS = {
     'password': '12345'
 }
 
+# Пароли
+# Движок            PostgreSQL
+# Сервер            db
+# Имя пользователя  root
+# Пароль            12345
+# База данных       postgres
+
 # Telegram bot token
 TELEGRAM_TOKEN = "6594195662:AAF0Ssws8t0EzSvnjOpEHymHr4P0L6kgRr8"
